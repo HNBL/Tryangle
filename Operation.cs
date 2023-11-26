e5857e920c21bb8a330835968b303a2d6b94fe47
HNBL <DerKudinov@gmail.com>
2023-11-27 00:57:58 +0300

    Add project files.

---
+++ Operation.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tryangle
+{
+
+
+
+
+    public class Square
+    {
+
+
+        public static void Tryangle(int side)
+        {
+
+            double P = side * 3;//значение периметра
+            double Side;
+            Side = P / 3; //Значение строны
+            
+
+            double p2 = P / 2 - Side;//Полупериметр
+            double pow = Math.Pow(p2, 3);// возведения в куб по формуле Герона
+            double s = pow * P / 2;
+            double S = Math.Sqrt(s);//корень крвадратный по формуле герона
+
+            Console.WriteLine("Сторона   Площадь\n {0: 0.00}   {1: 0.00}", Side, S);
+
+            
+        }
+
+        public static void Tryangle(double a,double b, double c) 
+        {
+
+            if (Square.TryangleExit(a,b,c))
+            {
+
+
+                double p = (a + b + c) / 2; ///значенее полупериметра
+
+                double p2 = p * (p - a) * (p - b) * (p - c);
+                double S = Math.Sqrt(p2);
+                Console.WriteLine("Сторона А, Сторона Б, Сторона С  Площадь\n {0: 0.00}      {1: 0.00}     {2: 0.00}    {3: 0.00}", a, b, c, S);
+
+            }
+            else
+            {
+                Console.WriteLine("Треугольник не существует, повторите ввод значений");
+
+            }
+
+        }
+
+        private static bool TryangleExit(double a, double b, double c)
+        {
+
+            if (a > 0 && b > 0 && c > 0)
+            {
+                return true;
+            }
+
+            else return false;
+        }
+
+    }
+
+}
+
---
+++ Tryangle.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Tryangle;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Треугольник равностороний? \n [y/n]?");
+
+        string answer = Convert.ToString(Console.ReadLine()); 
+
+        switch (answer) 
+        {
+            case "y":
+               Console.WriteLine("Ведите значение стороны равностороннего треугольника ");
+               
+                int side=int.Parse(Console.ReadLine());
+                Square.Tryangle(side);
+
+                break;
+            default:
+                Console.WriteLine("Введите три стороны треугольнка?");
+                int a=int.Parse(Console.ReadLine());
+                int b = int.Parse(Console.ReadLine());
+                int c = int.Parse(Console.ReadLine());
+               
+                Square.Tryangle (a,b,c);
+
+                break;
+        
+        
+        
+        }
+
+    }
+}
+
+
